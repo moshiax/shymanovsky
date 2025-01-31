31102f5f1ab13b59ee450e4f039540fe8e22bcfe
sheva <olegbeznogy@gmail.com>
2025-01-31 11:18:38 +0300

    паук топотун

---
+++ shymanovsky.mooo.com/banner.js
@@ -0,0 +1,233 @@
+var banner = new Array()
+
+banner[0] = new Array()
+banner[0][0] = "http://shymanovsky.mooo.com/assange_pauk.jpg"
+banner[0][1] = "http://shymanovsky.mooo.com/topotun.html"
+banner[0][2] = "Украине угрожает Паук-Топотун"
+
+banner[1] = new Array()
+banner[1][0] = "http://shymanovsky.mooo.com/assange_experiment.jpg"
+banner[1][1] = "http://pan-szymanowski.livejournal.com/843459.html"
+banner[1][2] = "В Тихом Океане возникнет Архипелаг Гулаг"
+
+banner[2] = new Array()
+banner[2][0] = "http://shymanovsky.mooo.com/assange_sokrov.jpg"
+banner[2][1] = "http://shymanovsky.mooo.com/sokro.html"
+banner[2][2] = "Сокровища Украины затоплены в Черном Море"
+
+banner[3] = new Array()
+banner[3][0] = "http://shymanovsky.mooo.com/assange_balalaika.jpg"
+banner[3][1] = "http://pan-szymanowski.livejournal.com/818061.html"
+banner[3][2] = "Семьдесят лет запрету балалайки в США"
+
+banner[4] = new Array()
+banner[4][0] = "http://shymanovsky.mooo.com/assange_bomba.jpg"
+banner[4][1] = "http://shymanovsky.mooo.com/abo.html"
+banner[4][2] = "Атомная бомба - это очень просто"
+
+banner[5] = new Array()
+banner[5][0] = "http://shymanovsky.mooo.com/assange_putin.jpg"
+banner[5][1] = "http://shymanovsky.mooo.com/putin.html"
+banner[5][2] = "Мои встречи с Владимиром Путиным"
+
+banner[6] = new Array()
+banner[6][0] = "http://shymanovsky.mooo.com/assange_generator.jpg"
+banner[6][1] = "http://shymanovsky.mooo.com/wikileaks.html"
+banner[6][2] = "Генератор секретных утечек Wikileaks"
+
+banner[7] = new Array()
+banner[7][0] = "http://shymanovsky.mooo.com/assange_pravoza.jpg"
+banner[7][1] = "http://shymanovsky.mooo.com/pravoza.html"
+banner[7][2] = "Темная сторона правозащитников"
+
+banner[8] = new Array()
+banner[8][0] = "http://shymanovsky.mooo.com/assange_piraty.jpg"
+banner[8][1] = "http://pan-szymanowski.livejournal.com/1188059.html"
+banner[8][2] = "Украинские пираты свирепствуют в Черном Море"
+
+banner[9] = new Array()
+banner[9][0] = "http://shymanovsky.mooo.com/assange_zombi.jpg"
+banner[9][1] = "http://shymanovsky.mooo.com/zombi.html"
+banner[9][2] = "Искусство зомбирования в Интернете"
+
+banner[10] = new Array()
+banner[10][0] = "http://shymanovsky.mooo.com/trump_mozart.jpg"
+banner[10][1] = "http://shymanovsky.mooo.com/mozart.html"
+banner[10][2] = "Музыкальный генератор Моцарта"
+
+banner[11] = new Array()
+banner[11][0] = "http://shymanovsky.mooo.com/assange_ukro.jpg"
+banner[11][1] = "http://www.silanaroda.com/forum/read.php?1,158626"
+banner[11][2] = "О самоидентификации украинцев"
+
+banner[12] = new Array()
+banner[12][0] = "http://shymanovsky.mooo.com/assange_darvin.jpg"
+banner[12][1] = "http://shymanovsky.mooo.com/otjig.html"
+banner[12][2] = "Номинации на премию Дарвина от правозащитных организаций России и СНГ"
+
+
+banner[13] = new Array()
+banner[13][0] = "http://shymanovsky.mooo.com/assange_stalin.jpg"
+banner[13][1] = "http://shymanovsky.mooo.com/stalin.html"
+banner[13][2] = "Тело Сталина хранится на Украине"
+
+banner[14] = new Array()
+banner[14][0] = "http://shymanovsky.mooo.com/assange_stalin1.jpg"
+banner[14][1] = "http://shymanovsky.mooo.com/stalin.html"
+banner[14][2] = "Тело Сталина хранится на Украине"
+
+banner[15] = new Array()
+banner[15][0] = "http://shymanovsky.mooo.com/nahuatl.jpg"
+banner[15][1] = "http://shymanovsky.mooo.com/nahuatl.html"
+banner[15][2] = "Курс языка Науатль"
+
+banner[16] = new Array()
+banner[16][0] = "http://shymanovsky.mooo.com/assange_tlahtoani.jpg"
+banner[16][1] = "http://shymanovsky.mooo.com/tlahtoani.html"
+banner[16][2] = "Надпись на плите Мавзолея"
+
+banner[17] = new Array()
+banner[17][0] = "http://shymanovsky.mooo.com/snowden_sta_duh.jpg"
+banner[17][1] = "http://shymanovsky.mooo.com/sta_duh.html"
+banner[17][2] = "Традиции спиритизма в СССР"
+
+banner[18] = new Array()
+banner[18][0] = "http://shymanovsky.mooo.com/snowden_liberpost.jpg"
+banner[18][1] = "http://shymanovsky.mooo.com/liberpost.html"
+banner[18][2] = "Генератор правозащитных текстов"
+
+banner[19] = new Array()
+banner[19][0] = "http://shymanovsky.mooo.com/snowden_svobodak.jpg"
+banner[19][1] = "http://shymanovsky.mooo.com/svobodak.html"
+banner[19][2] = "Разгон Радио Свобода"
+
+banner[20] = new Array()
+banner[20][0] = "http://shymanovsky.mooo.com/snowden_putinomer.jpg"
+banner[20][1] = "http://shymanovsky.mooo.com/r_putin.html"
+banner[20][2] = "Путиномер Шимановского"
+
+banner[21] = new Array()
+banner[21][0] = "http://shymanovsky.mooo.com/snowden_radio_liberpost.jpg"
+banner[21][1] = "http://shymanovsky.mooo.com/radio_liberpost.html"
+banner[21][2] = "Радио Liberpost"
+
+banner[22] = new Array()
+banner[22][0] = "http://shymanovsky.mooo.com/lscdem.jpg"
+banner[22][1] = "http://topotun-spider.mooo.com/scd.html"
+banner[22][2] = "International Silver Cleaning Day"
+
+banner[23] = new Array()
+banner[23][0] = "http://shymanovsky.mooo.com/snowden_ukra.jpg"
+banner[23][1] = "http://shymanovsky.mooo.com/ukra.html"
+banner[23][2] = "Некоторые аспекты европезации Крыма"
+
+banner[24] = new Array()
+banner[24][0] = "http://shymanovsky.mooo.com/snowden_putinbog.jpg"
+banner[24][1] = "http://shymanovsky.mooo.com/putinbog.html"
+banner[24][2] = "Путин - это Бог?"
+
+banner[25] = new Array()
+banner[25][0] = "http://shymanovsky.mooo.com/snowden_shron.jpg"
+banner[25][1] = "http://pan-szymanowski.livejournal.com/1270900.html"
+banner[25][2] = "Как устроить схрон"
+
+banner[26] = new Array()
+banner[26][0] = "http://shymanovsky.mooo.com/lscdm.jpg"
+banner[26][1] = "http://topotun-spider.mooo.com/scd.html"
+banner[26][2] = "Международный день чистки серебра"
+
+banner[27] = new Array()
+banner[27][0] = "http://shymanovsky.mooo.com/snowden_fsb.jpg"
+banner[27][1] = "http://shymanovsky.mooo.com/fsb.html"
+banner[27][2] = "Секреты работы ФСБ в интернете"
+
+banner[28] = new Array()
+banner[28][0] = "http://shymanovsky.mooo.com/trump_topotun.jpg"
+banner[28][1] = "http://topotun-spider.mooo.com"
+banner[28][2] = "Официальный сайт Паука Топотуна"
+
+banner[29] = new Array()
+banner[29][0] = "http://shymanovsky.mooo.com/trump_necrodesign.jpg"
+banner[29][1] = "http://necrodesign.hldns.ru"
+banner[29][2] = "Севастопольская студия некродизайна"
+
+banner[30] = new Array()
+banner[30][0] = "http://shymanovsky.mooo.com/trump_gimnua.jpg"
+banner[30][1] = "http://shymanovsky.mooo.com/gimnua.html"
+banner[30][2] = "Целебные свойства гимна Украины"
+
+banner[31] = new Array()
+banner[31][0] = "http://shymanovsky.mooo.com/trump_el_stalin.jpg"
+banner[31][1] = "http://shymanovsky.mooo.com/el_stalin.html"
+banner[31][2] = "Электронный Сталин"
+
+banner[32] = new Array()
+banner[32][0] = "http://shymanovsky.mooo.com/trump_fl.jpg"
+banner[32][1] = "http://shymanovsky.mooo.com/fl.html"
+banner[32][2] = "Самоучитель игры на свирели"
+
+banner[33] = new Array()
+banner[33][0] = "http://shymanovsky.mooo.com/trump_yogtze.jpg"
+banner[33][1] = "http://shymanovsky.mooo.com/yogtze.html"
+banner[33][2] = "Дело YOGTZE. Разгадка"
+
+banner[34] = new Array()
+banner[34][0] = "http://shymanovsky.mooo.com/trump_is12n3.jpg"
+banner[34][1] = "http://shymanovsky.mooo.com/is12n3.html"
+banner[34][2] = "is12n3.pdf Секретный файл ЦРУ"
+
+banner[35] = new Array()
+banner[35][0] = "http://shymanovsky.mooo.com/trump_ukra.jpg"
+banner[35][1] = "http://shymanovsky.mooo.com/ukra.html"
+banner[35][2] = "Некоторые аспекты европезации Крыма"
+
+banner[36] = new Array()
+banner[36][0] = "http://shymanovsky.mooo.com/assange2_darknet.jpg"
+banner[36][1] = "http://ma3hqhoccgsy5cwdtfpdorwgryarlgq63da2fy4xpgrouqmw2f77wkid.onion"
+banner[36][2] = "Даркнет против демократии"
+
+banner[37] = new Array()
+banner[37][0] = "http://shymanovsky.mooo.com/trump_shentro.jpg"
+banner[37][1] = "https://zrada.envs.net/"
+banner[37][2] = "Коды шентропье ежедневно"
+
+banner[38] = new Array()
+banner[38][0] = "http://shymanovsky.mooo.com/trump_sepuku.jpg"
+banner[38][1] = "http://shymanovsky.mooo.com/sepuku.html"
+banner[38][2] = "Как правильно выполнить харакири"
+
+banner[39] = new Array()
+banner[39][0] = "http://shymanovsky.mooo.com/trump_jupel.jpg"
+banner[39][1] = "http://jupel.kozow.com/"
+banner[39][2] = "Как изготовть жупел"
+
+banner[40] = new Array()
+banner[40][0] = "http://shymanovsky.mooo.com/karl_naval.jpg"
+banner[40][1] = "http://shymanovsky.mooo.com/naval.html"
+banner[40][2] = "Неудачный побег Навального"
+
+banner[41] = new Array()
+banner[41][0] = "http://shymanovsky.mooo.com/karl_russia.jpg"
+banner[41][1] = "http://shymanovsky.mooo.com/r_russia.html"
+banner[41][2] = "Индекс русофобии"
+
+banner[42] = new Array()
+banner[42][0] = "http://shymanovsky.mooo.com/karl_uchkuduk.jpg"
+banner[42][1] = "http://shymanovsky.mooo.com/uchkuduk.html"
+banner[42][2] = "Зловещая песня Учкудук"
+
+banner[43] = new Array()
+banner[43][0] = "http://shymanovsky.mooo.com/karl_piramidon.jpg"
+banner[43][1] = "http://shymanovsky.mooo.com/piramidon.html"
+banner[43][2] = "Кровавая история амидопирина"
+
+
+n = Math.random()*44
+nb = Math.floor(n)
+
+//document.write('<center>')
+document.write('<a href=' + banner[nb][1] + ' target= _blank>')
+document.write('<img src=' + banner[nb][0] + ' alt=\"' + banner[nb][2] + '\" border=0 > </a>')
+//document.write('</center>')
+
+
